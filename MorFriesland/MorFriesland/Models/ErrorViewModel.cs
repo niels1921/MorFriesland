807bccb228a7c115924b67491a52e2b5f9b1e0d3
niels1921 <nielsie21@live.nl>
2018-03-19 12:12:52 +0100

    First

---
+++ MorFriesland/MorFriesland/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace MorFriesland.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
