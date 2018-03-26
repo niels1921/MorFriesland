72f934af7241addfbfc7fa3b9e73344bb300cca8
Klaas058 <werk1702@student.nhl.nl>
2018-03-26 11:20:35 +0200

    Email

--- MorFriesland/MorFriesland/Program.cs
+++ MorFriesland/MorFriesland/Program.cs
@@ -7,6 +7,10 @@ using Microsoft.AspNetCore;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Logging;
+using SendGrid;
+using SendGrid.Helpers.Mail;
+using System;
+using System.Threading.Tasks;
 
 namespace MorFriesland
 {
@@ -14,9 +18,22 @@ namespace MorFriesland
     {
         public static void Main(string[] args)
         {
+            Execute().Wait();
             BuildWebHost(args).Run();
         }
 
+        static async Task Execute()
+        {
+            var apiKey = Environment.GetEnvironmentVariable("SENDGRID_KEY", EnvironmentVariableTarget.User);
+            var client = new SendGridClient(apiKey);
+            var from = new EmailAddress("klaas.vanderwerk@gmail.com", "Example User");
+            var subject = "Sending with SendGrid is Fun";
+            var to = new EmailAddress("klaas.vanderwerk@gmail.com", "Example User");
+            var plainTextContent = "and easy to do anywhere, even with C#";
+            var htmlContent = "<strong>and easy to do anywhere, even with C#</strong>";
+            var msg = MailHelper.CreateSingleEmail(from, to, subject, plainTextContent, htmlContent);
+            var response = await client.SendEmailAsync(msg);
+        }
         public static IWebHost BuildWebHost(string[] args) =>
             WebHost.CreateDefaultBuilder(args)
                 .UseStartup<Startup>()
