bbe7ca53993b07b3f05014404e4b84ce800265c4
Klaas058 <werk1702@student.nhl.nl>
2018-03-27 14:38:38 +0200

    Revert "Mail naar bronhouder"
    This reverts commit 45d6cf842a6cc5a97e763e4ac8266525812e2b56.

--- MorFriesland/MorFriesland/Startup.cs
+++ MorFriesland/MorFriesland/Startup.cs
@@ -32,6 +32,7 @@ namespace MorFriesland
             }
         }
 
+
         public IConfiguration Configuration { get; }
 
         // This method gets called by the runtime. Use this method to add services to the container.
