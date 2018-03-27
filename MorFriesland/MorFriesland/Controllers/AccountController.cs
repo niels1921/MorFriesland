d48e52df83db1547b551e111b204e5a141455e3f
Klaas058 <werk1702@student.nhl.nl>
2018-03-27 11:03:59 +0200

    half email

--- MorFriesland/MorFriesland/Controllers/AccountController.cs
+++ MorFriesland/MorFriesland/Controllers/AccountController.cs
@@ -234,12 +234,12 @@ namespace MorFriesland.Controllers
                     var callbackUrl = Url.EmailConfirmationLink(user.Id, code, Request.Scheme);
                     await _emailSender.SendEmailConfirmationAsync(model.Email, callbackUrl);
 
-                    if (!User.IsInRole("test"))
+                    if (!User.IsInRole("User"))
                     {
-                        await _userManager.AddToRoleAsync(user, "test");
+                        await _userManager.AddToRoleAsync(user, "User");
                     }
 
-                    await _signInManager.SignInAsync(user, isPersistent: false);
+                    //await _signInManager.SignInAsync(user, isPersistent: false);
                     _logger.LogInformation("User created a new account with password.");
                     return RedirectToLocal(returnUrl);
                 }
