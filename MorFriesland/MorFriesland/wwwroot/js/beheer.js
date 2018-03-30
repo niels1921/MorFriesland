9a7c3bb844f0336e75be96bfe35f8f17f0e0ea0d
niels1921 <nielsie21@live.nl>
2018-03-30 17:23:19 +0200

    styling

---
+++ MorFriesland/MorFriesland/wwwroot/js/beheer.js
@@ -0,0 +1,92 @@
+﻿//Variabelen die later in de code gevuld worden;
+
+
+
+defaultMap();
+
+//Laad de map in
+function initMap() {
+    var fryslan = { lat: 53.1641642, lng: 5.7817542 };
+    var map = new google.maps.Map(document.getElementById('map'), {
+        zoom: 10,
+        center: fryslan
+    });
+    var icon = {
+        url: "/images/Pompebledblauw.png",
+        scaledSize: new google.maps.Size(35, 35), // scaled size
+        origin: new google.maps.Point(0, 0), // origin
+        anchor: new google.maps.Point(0, 0) // anchor
+    };
+
+    var icon2 = {
+        url: "/images/Pompebled.png",
+        scaledSize: new google.maps.Size(35, 35), // scaled size
+        origin: new google.maps.Point(0, 0), // origin
+        anchor: new google.maps.Point(0, 0) // anchor
+    };
+
+    Locationmarker = new google.maps.Marker({
+        map: map,
+        draggable: false,
+        icon: icon
+    });
+
+    $('#meldingen[data-lat]').each(function () {
+
+        var latdata = $(this).data('lat');
+        var lngdata = $(this).data('lng');
+        var Name = $(this).data('name');
+        var Beschrijving = $(this).data('beschrijving');
+        var Img = $(this).data('img');
+
+        var content = "";
+        if (Img !== "/uploads/" + Name + "/") {
+            content = "<div class='col-md-12 nopadding'><div class='col-md-4 nopadding'><img style='max-width: 100%;' src='" + Img + "'></div><div class='col-md-8 infobeschrijving'><b>" + Name + "</b><br/>" + Beschrijving + "</div></div>";
+        } else {
+            content = "<div class='col-md-12 nopadding'><b>" + Name + "</b><br/>" + Beschrijving + "</div>";
+        }
+
+
+
+        var marker = new google.maps.Marker({
+            position: { lat: latdata, lng: lngdata },
+            title: 'Home Center',
+            map: map,
+            icon: icon2
+
+        });
+
+        var infoWindow = new google.maps.InfoWindow({
+            content: content,
+            maxWidth: 400
+
+        });
+
+        google.maps.event.addListener(marker, 'click', function () {
+            infoWindow.open(map, marker);
+        });
+
+    });
+
+
+    
+}
+
+function toggleBounce() {
+
+
+    if (Locationmarker.getAnimation() !== null) {
+        Locationmarker.setAnimation(null);
+    } else {
+        Locationmarker.setAnimation(google.maps.Animation.BOUNCE);
+    }
+}
+
+
+
+
+
+
+function defaultMap() {
+   
+}
--- MorFriesland/MorFriesland/wwwroot/js/site.js
+++ MorFriesland/MorFriesland/wwwroot/js/site.js
@@ -2,14 +2,13 @@
 
 // Write your JavaScript code.
 
-$(document).on('change', ':file', function () {
-    var input = $(this),
-        numFiles = input.get(0).files ? input.get(0).files.length : 1,
-        label = input.val().replace(/\\/g, '/').replace(/.*\//, '');
-    input.trigger('fileselect', [numFiles, label]);
-});
 
 $(document).ready(function () {
+
+    $(".navbar-toggle").on("click", function () {
+        $(this).toggleClass("active");
+    });
+
     $(".loader").hide();
 
 
@@ -17,7 +16,7 @@ $(document).ready(function () {
     $("#EmailCheckbox").click(function () {
         $("#email").toggle();
 
-        if ($(".ingelogd").data('ingelogd') == "loggedin") {
+        if ($(".ingelogd").data('ingelogd') === "loggedin") {
             if ($('#EmailCheckbox').is(':unchecked')) {
                 $("#email").val("false");
                 console.log("false");
