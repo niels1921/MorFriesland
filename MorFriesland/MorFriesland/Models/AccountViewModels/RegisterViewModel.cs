40931909710d45ff4202f3561c77cb2f74575375
Klaas058 <werk1702@student.nhl.nl>
2018-04-04 15:39:57 +0200

    Register view model validation message

--- MorFriesland/MorFriesland/Models/AccountViewModels/RegisterViewModel.cs
+++ MorFriesland/MorFriesland/Models/AccountViewModels/RegisterViewModel.cs
@@ -9,19 +9,19 @@ namespace MorFriesland.Models.AccountViewModels
     public class RegisterViewModel
     {
         [Required]
-        [EmailAddress]
+        [EmailAddress(ErrorMessage ="Voer een geldig e-mail adres in")]
         [Display(Name = "Email")]
         public string Email { get; set; }
 
         [Required]
-        [StringLength(100, ErrorMessage = "The {0} must be at least {2} and at max {1} characters long.", MinimumLength = 1)]
+        [StringLength(100, ErrorMessage = "Het {0} moet minstens {2} en maximaal {1} karakters zijn.", MinimumLength = 1)]
         [DataType(DataType.Password)]
         [Display(Name = "Password")]
         public string Password { get; set; }
 
         [DataType(DataType.Password)]
-        [Display(Name = "Confirm password")]
-        [Compare("Password", ErrorMessage = "The password and confirmation password do not match.")]
+        [Display(Name = "Bevestig wachtwoord")]
+        [Compare("Password", ErrorMessage = "Het wachtwoord en de bevestiging komen niet overeen")]
         public string ConfirmPassword { get; set; }
     }
 }
